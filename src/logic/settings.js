89feea5bde806d7e7a6d1d05b384f2ee4da0ce8e
ludwing.cano <ludwing238@gmail.com>
2023-06-09 13:02:41 -0600

    update settings

--- src/logic/settings.js
+++ src/logic/settings.js
@@ -5,16 +5,17 @@ async function checkSettings(outputs, settings) {
     if (!settings || settings.length == 0) {
         return outputs[0];
     }
-
+    let filterOutputs = outputs[0];
+    
     settings.forEach(element => {
         switch (element.key) {
             case "campaigns_per_user":
-                outputs = outputs.slice(0, element.value);
+                filterOutputs = outputs[0].slice(0, element.value);
                 break;
         }
     });
 
-    return outputs[0];
+    return filterOutputs;
 
 }
 
